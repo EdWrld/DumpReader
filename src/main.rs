216383e998a997b18ca84208bcd986f9e7ae4786
EdWorld <edwardutria12@gmail.com>
2022-03-20 01:33:45 -0400

    Added -v -h functionality

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,37 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // args() creates a iterator, .collect() makes it a collection 
     let stdin = io::stdin();
 
+    // println!("{:?}", args);
+
     let mut content: String = String::new();  
-    
-    if args.len() == 2 {        
+
+    if args.len() >= 2 && &args[1][0..2] != "-" {      
         content = fs::read_to_string(&args[1]) // filename
         .expect("Something went wrong when reading file");
     }
-    else{
+    else {
         for line in stdin.lock().lines(){
             let line = line.expect("Could not read line from standard in");
             content.push_str(&line);
             content.push_str("\n");
-        } 
+        }
     }
    
-    println!("\n{}",content);
+   println!("{}",content);
+   
+    for arg in args {
+        if arg == "-v" || arg == "-V"{
+            println!("\nVersion 1.0");
+        }
+        else if arg == "-h" || arg == "-H"{
+            println!("\nInput a your dumpfile or pipe it when before cargo run \nExample: cargo run filename.txt or filename.txt | cargo run \n-v - version # \n-h - help");
+        }
+    }
 
-    
-        // println!("{:?}", args); // println! is a macro not a function, so it rewrites the code on runtime
-        // println!("\n{}",content);
+    // println!("\n{}",content);
     
 }
 // If you run into the linker cc issue its because you don't have gcc installed 
 // To read the file you need to do cargo run in the same location as the Cargo.toml file
+// println! is a macro not a function, so it rewrites the code at runtime
 
